a98d7663ff553cce9f716e47351ca203a9a08873
Joel Bernasky <joel_bernasky@hotmail.com>
2019-10-23 11:35:04 -0400

    event emitters

--- ngs/6-modules/7-event-emitters/example.js
+++ ngs/6-modules/7-event-emitters/example.js
@@ -1,4 +1,22 @@
 const EventEmitter = require('events');
 
+const myEmitter = new EventEmitter();
+
+setImmediate(() => {
+    myEmitter.emit('TEST_EVENT');
+}); 
+
+myEmitter.on('TEST_EVENT', () => {
+    console.log('TEST_EVENT was fired');
+});
+myEmitter.on('TEST_EVENT', () => {
+    console.log('TEST_EVENT was fired');
+});
+myEmitter.on('TEST_EVENT', () => {
+    console.log('TEST_EVENT was fired');
+});
+
+// myEmitter.emit('TEST_EVENT');
+
 // Streams are Event Emitters
 // process.stdin, process.stdout
