51c36d0d0f8521681cf0b878144c339ba055de76
Joel Bernasky <joel_bernasky@hotmail.com>
2019-10-23 10:57:46 -0400

    errors

--- ngs/6-modules/5-errors/2-try.js
+++ ngs/6-modules/5-errors/2-try.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const fs = require('fs');
 
-const files = ['.bash_profile', 'kjkjhh', '.npmrc'];
+const files = ['.bash_history', 'kjkjyy', '.yarnrc'];
 
 files.forEach(file => {
   try {
     const filePath = path.resolve(process.env.HOME, file);
-    const data = fs.readFileSync(filePath);
+    const data = fs.readFileSync(filePath, 'utf-42');
     console.log('File data is', data);
   } catch (err) {
-    console.log('File not found');
+    console.log('Something went wrong and we are going to ignore it.');
   }
 });
--- ngs/6-modules/5-errors/3-throw.js
+++ ngs/6-modules/5-errors/3-throw.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const fs = require('fs');
 
-const files = ['.bash_profile', 'kjkjhh', '.npmrc'];
+const files = ['.bash_history', 'kjkjyy', '.yarnrc'];
 
 files.forEach(file => {
   try {
     const filePath = path.resolve(process.env.HOME, file);
-    const data = fs.readFileSync(filePath);
+    const data = fs.readFileSync(filePath, 'asdf');
     console.log('File data is', data);
   } catch (err) {
     if (err.code === 'ENOENT') {
